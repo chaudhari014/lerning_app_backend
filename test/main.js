659e1bca9bb839852241364c8f2a17a6a17d7f8f
chaudhari014 <fatarambhai.k14@gmail.com>
2024-02-18 22:38:38 +0530

    update

--- test/main.js
+++ test/main.js
@@ -1,4 +1,15 @@
-let a=20;
-let b=10;
+const fs=require("fs")
 
-export {a,b}
+const stream=fs.createReadStream("../index.js")
+
+stream.on("data",(chunk)=>{
+    console.log(chunk.toString())
+})
+stream.on("end",()=>{
+    console.log("end ")
+})
+
+console.log("rahul")
+setTimeout(()=>{
+    console.log("rahul2")
+},1)
